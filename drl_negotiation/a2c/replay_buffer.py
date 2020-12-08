5f41c083eeba6953df3d4c68a8f55756ca27b05b
naodongbanana <n1085633848@outlook.com>
2020-12-08 16:32:16 +0100

    Create Prioritized Replay buffer

---
+++ drl_negotiation/a2c/replay_buffer.py
@@ -0,0 +1,57 @@
+import numpy as np 
+import random 
+
+class ReplayBuffer(object):
+
+    def __init__(self, size):
+        """
+        prioritized replay buffer
+        """
+
+        self._storage = []
+        self._maxsize = int(size)
+        self._next_idx = 0
+
+    def __len__(self):
+        return len(self._storage)
+
+    def clear(self):
+        self._storage = []
+        self._next_idx = 0
+
+    def add(self, obs_t, action, reward, obs_tp1, done):
+        data = (obs_t, action, reward, obs_tp1, done)
+        
+        if self._next_idx >=len(self._storage):
+            self._storage.append(data)
+        else:
+            self._storage[self._next_idx] = data
+        self._next_idx = (self._next_idx + 1) % self._maxisze
+
+    def make_index(self, batch_size):
+        return [random.randint(0, len(self._storage) -1) for _ in range(batch_size)]
+    
+    def sample_index(self, idx):
+        return self._encode_sample(idx)
+    
+    def sample(self, batch_size):
+        if batch_size >0:
+            idxes = self.make_index(batch_size)
+        else:
+            idxes = range(0, len(self._storage))
+        return self._conde_sample(idxes)
+
+    def collect(self):
+        return self.sample(-1)
+
+    def _encode_sample(self, idxes):
+        obses_t, actions, rewards, obses_tp1, dones = [], [], [], [], []
+        for i in idxes:
+            data = self._storage[i]
+            obs_t, action, reward, obs_tp1, done = data
+            obses_t.append(np.array(obs_t, copy=False))
+            actions.append(np.array(actino, copy=False))
+            rewards.append(reward)
+            obses_tp1.append(np.array(obs_tp1, copy=False))
+            dones.append(done)
+        return np.array(obses_t), np.array(actions), np.array(rewards), np.array(obses_tp1), np.array(dones)
