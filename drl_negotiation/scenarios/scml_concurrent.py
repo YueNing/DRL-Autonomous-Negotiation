1bb8220923cbaae8218dda4a75193a4cf1d14210
naodongbanana <n1085633848@outlook.com>
2021-02-01 01:45:12 +0100

    [feat] Fixed more parameters of TrainWorld
    - improve the logic of resetting of world

--- drl_negotiation/scenarios/scml_concurrent.py
+++ drl_negotiation/scenarios/scml_concurrent.py
@@ -5,6 +5,7 @@ from drl_negotiation.scenarios.scenario import BaseScenario
 from drl_negotiation.core.hyperparameters import *
 from drl_negotiation.utils.utils import make_world
 from scml import SCML2020World
+from drl_negotiation.core.core import TrainWorld
 from drl_negotiation.core.core import MySCML2020Agent, TrainWorld
 import numpy as np
 import random
@@ -15,21 +16,8 @@ class Scenario(BaseScenario):
         world = make_world(config=config)
         return world
 
-    def reset_world(self, world):
-        agent_types = world.configuration['agent_types']
-        agent_params = world.configuration['agent_params'][:-2]
-        n_steps = world.configuration['n_steps']
-
-        reset_configuration = SCML2020World.generate(
-            agent_types=agent_types,
-            agent_params=agent_params,
-            n_steps=n_steps
-        )
-
-        reset_configuration['negotiation_speed'] = world.configuration['negotiation_speed'] \
-            if "negotiation_speed" in world.configuration else NEGOTIATION_SPEED
-
-        world.__init__(configuration=reset_configuration)
+    def reset_world(self, world:TrainWorld):
+        world.__init__(configuration=world.configuration)
 
     def observation(self, agent: MySCML2020Agent, world: TrainWorld, seller=True):
         last_offers = [[] for _ in agent.awi.my_consumers] + [[] for _ in agent.awi.my_suppliers]
